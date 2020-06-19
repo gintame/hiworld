d2b289ceda17cb2cb6461f2130332fc687a325e4
bai <2233251653@qq.com>
2020-06-20 00:31:29 +0800

    new test

--- src/HelloServlet.java
+++ src/HelloServlet.java
@@ -12,6 +12,7 @@ public class HelloServlet extends HttpServlet{
         try {
             response.getWriter().println("<h1>Hello Servlet!</h1>");
             response.getWriter().println(new Date().toLocaleString());
+            System.out.println("ooooooooooooooo");
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
